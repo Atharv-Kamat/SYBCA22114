f2e5e55c1ccf2fc2b303a7d03f68a639f62ff2e2
Atharv kamat Bambolkar <rbca22114.rcca@gmail.com>
2024-01-03 09:14:31 +0530

    commit

---
+++ SYBCA-101/src/lecture2/BaseJavaProgram.java
@@ -0,0 +1,10 @@
+package lecture2;
+
+public class BaseJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
