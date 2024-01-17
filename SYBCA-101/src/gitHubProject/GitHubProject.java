00e005c71a092af91c3b018e574133acff44d77e
Atharv kamat Bambolkar <rbca22114.rcca@gmail.com>
2024-01-17 09:56:02 +0530

    commit

--- SYBCA-101/src/gitHubProject/GitHubProject.java
+++ SYBCA-101/src/gitHubProject/GitHubProject.java
@@ -7,13 +7,20 @@ public class GitHubProject {
 	public static void main(String[] args) {
 		try (// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in)) {
-			int a,b,sum;
+			int a,b,sum,sub;
 			System.out.println("Enter the two numbers: ");
 			a = sc.nextInt();
 			b = sc.nextInt();
 			sum = a + b;
 			System.out.println("The sum of two numbers is " + sum);
+			sub = display(a,b);
+			System.out.println("The subtraction of two numbers is " + sub);
 		}
 	}
 
+	public static int display(int a,int b) {
+		int s;
+		s = a - b;
+		return s;
+	}
 }
